b80367b073600ce68753217d17b75647fea71fe8
Reda3334 <ahmed.reda.makroum@estudiantat.upc.edu>
2025-03-18 20:02:02 +0100

    parte visual

--- src/app/gym/gym.component.ts
+++ src/app/gym/gym.component.ts
@@ -3,14 +3,13 @@ import { GymService } from '../services/gym.service';
 import { Gym } from '../models/gym.model';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-gym',
-  standalone: true,
   imports: [FormsModule, CommonModule],
+  standalone: true,
   templateUrl: './gym.component.html',
-  styleUrl: './gym.component.css'
+  styleUrls: ['./gym.component.css']
 })
 export class GymComponent implements OnInit {
   gyms: Gym[] = [];
@@ -34,37 +33,39 @@ export class GymComponent implements OnInit {
       }
     );
   }
-    // Crear un nuevo gimnasio
-    createGym(): void {
-      this.gymService.createGym(this.newGym).subscribe(
+
+  // Crear un nuevo gimnasio
+  createGym(): void {
+    this.gymService.createGym(this.newGym).subscribe(
+      (data) => {
+        this.gyms.push(data);
+        this.newGym = { id: 0, name: '', place: '', price: 0 }; // Resetear el formulario
+      },
+      (error) => {
+        console.error('Error al crear gimnasio:', error);
+      }
+    );
+  }
+
+  // Actualizar un gimnasio
+  updateGym(): void {
+    if (this.selectedGym) {
+      this.gymService.updateGym(this.selectedGym.id, this.selectedGym).subscribe(
         (data) => {
-          this.gyms.push(data);
-          this.newGym = { id: 0, name: '', place: '', price: 0 }; // Resetear el formulario
+          const index = this.gyms.findIndex((g) => g.id === data.id);
+          if (index !== -1) {
+            this.gyms[index] = data;
+          }
+          this.selectedGym = null; // Limpiar selección
         },
         (error) => {
-          console.error('Error al crear gimnasio:', error);
+          console.error('Error al actualizar gimnasio:', error);
         }
       );
     }
-  
-    // Actualizar un gimnasio
-    updateGym(): void {
-      if (this.selectedGym) {
-        this.gymService.updateGym(this.selectedGym.id, this.selectedGym).subscribe(
-          (data) => {
-            const index = this.gyms.findIndex((g) => g.id === data.id);
-            if (index !== -1) {
-              this.gyms[index] = data;
-            }
-            this.selectedGym = null; // Limpiar selección
-          },
-          (error) => {
-            console.error('Error al actualizar gimnasio:', error);
-          }
-        );
-      }
-    }
-      // Eliminar un gimnasio
+  }
+
+  // Eliminar un gimnasio
   deleteGym(id: number): void {
     this.gymService.deleteGym(id).subscribe(
       () => {
